3f380c6a3a2027afd616556d6184a1738c7a048f
mgkrishnamaddu <80996020+mgkrishnamaddu@users.noreply.github.com>
2021-03-20 11:11:35 +0530

    Add files via upload

---
+++ Vendor Machine/Vendor Machine/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vendor_Machine
+{
+    public class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> listProducts = new Dictionary<int, string>();
+            listProducts.Add(20, "Nickles");
+            listProducts.Add(10, "Dimes");
+            listProducts.Add(4, "Quarters");
+
+            Console.WriteLine("Please enter cost!");
+            int cost = Convert.ToInt32(Console.ReadLine());
+            if(listProducts.ContainsKey(cost))
+            {
+                Console.WriteLine(listProducts[cost].ToString());
+            }
+            else
+            {
+                Console.WriteLine("Invalid Coin, Rejected");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
